359d9885cc41486b80fd6a4f65b97bcd2afefbb4
Shreshtas4 <245122749036@mvsrec.edu.in>
2025-06-17 15:41:10 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,15 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Shreshta S";
+        int rollNo = 23;
+        String department = "Computer Science";
+        double cgpa = 8.7;
+
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Department: " + department);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
+
